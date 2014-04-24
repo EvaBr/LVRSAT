66b39b23c241ed95af0bd2698ada8df8a34254e7
Teja Tement <tement.teja@gmail.com>
2014-04-24 21:34:35 +0200

    Dodanih še nekaj praznih/osnovnih primerov.

--- testniprimeri.py
+++ testniprimeri.py
@@ -2,7 +2,9 @@
 #TESTNI PRIMERI
 #
 #Testni primeri za preizkusanje delovanja pretvorbe v CNF
-#obliko in resevanja z DPLL.
+#obliko in resevanja z DPLL. Vključeni so primeri preprostih
+#formul v različnih osnovnih možnih kombinacijah stavkov 'In',
+#'Ali', negacij ter možnih praznih stavkov/formul.
 #
 ##########################################################
 
@@ -16,11 +18,6 @@ x=Spr('x')
 y=Spr('y')
 z=Spr('z')
 w=Spr('w')
-
-xx=Lit('x')
-yy=Lit('y')
-zz=Lit('z')
-ww=Lit('w')
 
 primer1=Ali(x,y,z)
 print('===== Primer 1 =====')
@@ -94,22 +91,60 @@ print (primer12)
 print (primer12.cnf())
 print (dpll(primer12))
 
+#preverjanje dpll: primer brez čiste pojavitve
 primer13=Ali(In(x,y), Neg(x), Neg(y))
 print('===== Primer 13 =====')
 print (primer13)
 print (primer13.cnf())
 print (dpll(primer13))
 
+#preverjanje dpll: primer brez čiste pojavitve in brez stavkov dolžine 1
 primer14=Ali(In(x,y), In(Neg(x), Neg(y)))
 print('===== Primer 14 =====')
 print (primer14)
 print (primer14.cnf())
 print (dpll(primer14))
 
+#dolga čudna izpolnjiva formula :D
 primer15=Ali(In(Ali(Neg(x),y,z),In(x,w),z),Ali(w,Neg(z),Neg(w)),z,x)
 print('===== Primer 15 =====')
 print (primer15)
 print (primer15.cnf())
 print (dpll(primer15))
 
+primer16=In(x)
+print('===== Primer 16 =====')
+print (primer16)
+print (primer16.cnf())
+print (dpll(primer16))
 
+primer17=In()
+print('===== Primer 17 =====')
+print (primer17)
+print (primer17.cnf())
+print (dpll(primer17))
+
+primer18=Ali()
+print('===== Primer 18 =====')
+print (primer18)
+print (primer18.cnf())
+print (dpll(primer18))
+
+primer19=In(Ali(x, Neg(y), Neg(z)), Ali(Neg(x), y), Ali(x,z))
+print('===== Primer 19 =====')
+print (primer19)
+print (primer19.cnf())
+print (dpll(primer19))
+
+#dolga čudna neizpolnjiva formula :D
+primer20=In(Neg(x), y, Neg(Ali(z, Neg(w))), Neg(In(w,z)), Neg(Ali(w, y)))
+print('===== Primer 20 =====')
+print (primer20)
+print (primer20.cnf())
+print (dpll(primer20))
+
+primer21=Neg(Ali(x,y))
+print('===== Primer 21 =====')
+print (primer21)
+print (primer21.cnf())
+print (dpll(primer21))
