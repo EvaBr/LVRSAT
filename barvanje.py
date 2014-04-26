af601ee88a07859a59c838eda481f205ff74abba
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-04-26 02:33:06 +0200

    Popravljen bug, zdaj mora delati.

--- barvanje.py
+++ barvanje.py
@@ -39,7 +39,7 @@ def barvanje(n, E):
         tretja = In(tretja, In(Neg(In(Spr("c{0}.1".format(pov[1])),Spr("c{0}.1".format(pov[0])))),
                   Neg(In(Spr("c{0}.2".format(pov[1])),Spr("c{0}.2".format(pov[0])))),
         Neg(In(Spr("c{0}.3".format(pov[1])),Spr("c{0}.3".format(pov[0]))))))
-    tretja=tretja.poenostavi()
+    tretja = tretja.poenostavi()
     
     form = In(prva,druga,tretja)
     return form.poenostavi()
@@ -55,18 +55,18 @@ def kbarvanje(K, n, E):
 
     #vsako vozlišče je pobarvano vsaj z eno barvo
     for i in range(1,n+1):
-        prva = In(prva, *tuple(Ali(Spr(("c{0}."+str(j)).format(i)), Spr(("c{0}."+str(j)).format(i)), Spr(("c{0}."+str(j)).format(i))) for j in range (1,K+1)))
-    prva=prva.poenostavi()
+        prva = In(prva, Ali(*tuple(Spr(("c{0}.{1}").format(i,j)) for j in range (1,K+1))))
+    prva = prva.poenostavi()
 
     #eno vozlišče ni pobarvano z več kot eno barvo
     for i in range(1,n+1):
-        druga = In(druga, In(*tuple (Ali(Neg(Spr(("c{0}."+str(j)).format(i))),Neg(Spr(("c{0}."+str(k)).format(i)))) for j in range (1,K+1) for k in range (j+1,K+1))))
-    druga=druga.poenostavi()
+        druga = In(druga, In(*tuple (Ali(Neg(Spr(("c{0}.{1}").format(i,j))),Neg(Spr(("c{0}.{1}").format(i,k)))) for j in range (1,K+1) for k in range (j+1,K+1))))
+    druga = druga.poenostavi()
     
     #sosednji vozlišči sta različnih barv
     for pov in E:
-        tretja = In(tretja, In(*tuple(Ali(Neg(Spr(("c{0}."+str(j)).format(pov[1]))),Neg(Spr(("c{0}."+str(j)).format(pov[0])))) for j in range (1,K+1))))
-    tretja=tretja.poenostavi()
+        tretja = In(tretja, In(*tuple(Ali(Neg(Spr(("c{0}.{1}").format(pov[1],j))),Neg(Spr(("c{0}.{1}").format(pov[0],j)))) for j in range (1,K+1))))
+    tretja = tretja.poenostavi()
     
     form = In(prva,druga,tretja)
     return form.poenostavi()
