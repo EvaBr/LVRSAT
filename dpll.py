926242201d83c4f1c36ecb2e7ebc4deef9a12e84
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-05-01 19:51:02 +0200

    Olepsava, poenostavitev in pohitritev. Dela se zmeraj ne.

--- dpll.py
+++ dpll.py
@@ -5,26 +5,14 @@ from cnf import *
 
 
 def dodaj(el, vred, slov):  #Dela na CNF obliki, tj el je tipa Til/Lit, ne pa Spr/Neg. vred=T ali F, slovar={Spr("x"):T(),...}
-	""" Pomozna funkcija, ki preveri, ali je dan element v 
-	slovarju in ali ima ustrezno vrednost. Ce elementa v
-	slovarju ni, ga doda. Ce je, a ima napacno vrednost,
-	pa javi napako. Vnesen element mora biti v CNF obliki,
-	tj. tipa TIL ali LIT. """
+	""" Pomozna funkcija, ki v slovarju vrednosti spremenljivk pripise
+		podanemu elementu (ki mora biti tipa TIL ali LIT) ustrezno 
+		vrednost (glede na njegov tip). """
 
-	spr = Spr(el.ime)
+	spr = el.ime
 	if type(el)==Til:
-		vr = Neg(vred).poenostavi()
-		if spr in slov:
-			if slov[spr]==vr:
-				pass
-		else:
-			slov[spr] = vr
-	else: #if type = Lit
-		if spr in slov:
-			if slov[spr]==vred:
-				pass
-		else:
-			slov[spr] = vred
+		vred = Neg(vred).nnf()
+	slov[spr] = vred
 
 
 def zamenjaj(Fuormula, Abjikt, vridnastAbjikta):
@@ -52,7 +40,6 @@ def zamenjaj(Fuormula, Abjikt, vridnastAbjikta):
 					stavk -= 1
 					break
 			crka += 1
-
 		stavk += 1
 	#returnat nam ni treba nic, ker gre za spremembo formule na mestu.
 
@@ -60,75 +47,85 @@ def zamenjaj(Fuormula, Abjikt, vridnastAbjikta):
 
 def dpll(dieFormel):
 	""" Sprejme formulo v CNF obliki in pove, ali ji je mogoce zadostiti. 
-		Ce ji je, vrne slovar potrebnih vrednosti spremenljivk."""
+		Ce ji je, vrne slovar potrebnih vrednosti spremenljivk. """
 
-	slovar = {}
+	slovarc = {}
 	### Cista pojavitev:
 	pojavitve = {}
 	for stavk in dieFormel.stavki:
 		for lit in stavk.literali:
-			S = Spr(lit.ime)
+			S = lit.ime
 			if S in pojavitve:
 				pojavitve[S].add(type(lit))
 			else:
 				pojavitve[S] = {type(lit)}
-
 	for i in pojavitve:
 		if len(pojavitve[i])==1:
 			tip = pojavitve[i].pop()
-			dodaj(tip(i.ime), T(), slovar)
-
-	for neki in slovar:
-		zamenjaj(dieFormel, neki.ime, slovar[neki])
+			dodaj(tip(i.ime), T(), slovarc)
+	for neki in slovarc:
+		zamenjaj(dieFormel, neki, slovarc[neki])
 	
+	print("po čisti pojavitvi: ", len(dieFormel.stavki))
 	###
+
 	def pomozna(formula, slovar):
-		#formula = cnf oblike
+		#formula je cnf oblike
 		sprememba = True
-		while sprememba:			
+		while sprememba:
+			print("izvedba while zankice... ")
+			#print("formula ob zacetku zanke: ", formula)		
 			if formula.stavki==[]:
 				return (T(), slovar)
 			else:
 				for stavek in formula.stavki:
 					sprememba = False
 					if stavek.literali==[]:
+						print("prazn stavek!!")
 						return (F(), slovar)
 					elif len(stavek.literali)==1:  #Nasli smo stavek, ki je kar literal.
 						spremenljivka = stavek.literali[0]
 						#Nastavimo ustrezno vrednost spremenljivke:
 						dodaj(spremenljivka, T(), slovar)
-						zamenjaj(formula, spremenljivka.ime, slovar[Spr(spremenljivka.ime)])
+						zamenjaj(formula, spremenljivka.ime, slovar[spremenljivka.ime])
+						print("po zamenjavi spremenlj. "+spremenljivka.ime+" :  ", len(formula.stavki))
 						sprememba = True
 						break
 			
 		#Poglejmo, ali je ostala se kaksna spremenljivka brez vrednosti:
 		nasliNovo = False
 		for s in formula.stavki:  #Poisces eno spremenljivko, ki se ni v slovarju, tj. ji vrednost se ni dolocena.
+			print("furmula.stavk: ", s)
 			for l in s.literali:
 				nasliNovo = True
 				break
+			if nasliNovo: break
 	
-		if nasliNovo: 
-			formula.stavki.extend([Stavek([Lit(l.ime)])])
-			blabla = pomozna(formula, slovar)
+		if nasliNovo: #to bi se tako moralo zmeraj zgoditi, če pridemo do sem 
+
+			formula2 = Cnf([st for st in formula.stavki]) #naredimo kopijo, saj zadevo kasneje spreminjamo 'na mestu'
+			formula2.stavki.extend([Stavek([Lit(l.ime)])])
+			print("formula: ",len(formula.stavki),"  formula2: ",len(formula2.stavki))
+			
+			slovar2 = {i:slovar[i] for i in slovar} #naredimo kopijo, saj zadevo kasneje spreminjamo 'na mestu'
+			print("poskus z T():  ", len(formula2.stavki))
+			print("slovar za formulo2: ", slovar2)
+			blabla = pomozna(formula2, slovar2)
 			if blabla[0]==T():
 				return blabla
 			else:
-				#iz formule je treba stran vzet kar smo prej extendali, oz. menjat z lih negiranim stavkom
-				for sentence in formula.stavki:
-					sezn = sentence.literali
-					if len(sezn)==1 and sezn[0].ime==l.ime and type(sezn[0])==Lit:
-						sentence.literali[0] = Til(l.ime)		
+				formula.stavki.extend([Stavek([Til(l.ime)])])
+				print("Ni slo s T, poskus z F:  ", len(formula.stavki))
+				print("slovar za formulo: ", slovar)
 				return pomozna(formula, slovar)
-		else:
-			return (formula, slovar) #<-Ko pride do sem je formula ze T().cnf() ali F().cnf().  
 
-	rezultat = pomozna(dieFormel, slovar)
+	rezultat = pomozna(dieFormel, slovarc)
+	print("rezultat: ", rezultat)
 	
 	#Nastimamo se vse nepotrebne zadeve v slovarju:
 	for teja in pojavitve:
 		if teja not in rezultat[1]:
-			rezultat[1][teja] = T() #Smo optimisti, pa bomo vse nastimali na tru.
+			rezultat[1][teja] = T()
 
 	if rezultat[0]==T():
 		#print("Formula je izpolnljiva.")
