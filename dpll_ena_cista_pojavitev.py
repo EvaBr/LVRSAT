c5057ade23e4b330d32d961ae84ac771cfa24120
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-05-02 18:30:22 +0200

    Dodani verziji dpll z eno oz. brez ciste pojavitve, ter primerjava casov.

---
+++ dpll_ena_cista_pojavitev.py
@@ -0,0 +1,128 @@
+#implementacija DPLL brez (oz. z le eno) ciste pojavitve
+
+from boolean import *
+from cnf import *
+
+
+def dodaj(el, vred, slov):  #Dela na CNF obliki, tj el je tipa Til/Lit, ne pa Spr/Neg. vred=T ali F, slovar={Spr("x"):T(),...}
+        """ Pomozna funkcija, ki v slovarju vrednosti spremenljivk pripise
+                podanemu elementu (ki mora biti tipa TIL ali LIT) ustrezno 
+                vrednost (glede na njegov tip). """
+
+        spr = el.ime
+        if type(el)==Til:
+                vred = Neg(vred).poenostavi()
+        slov[spr] = vred
+
+
+def zamenjaj(Fuormula, Abjikt, vridnastAbjikta):
+        """ U Fuormuli (ka je u CNF abliki) zaminja use pojauitve 
+                Abjikta z vridnastjo tiha abjikta. (Na mistu!) 
+                Abjikt je padan kat string (sam z iminam). """
+
+        stavk = 0
+        stavkov = len(Fuormula.stavki)
+        while stavk < stavkov:
+                poved = Fuormula.stavki[stavk]
+                koliko = len(poved.literali)
+                crka = 0
+                while crka < koliko:
+                        naVrsti = poved.literali[crka]
+                        if naVrsti.ime == Abjikt:
+                                Tip = type(naVrsti)
+                                if (Tip==Til and vridnastAbjikta==T()) or (Tip==Lit and vridnastAbjikta==F()): #vstavljamo F v Ali -> lahko ga kar spustimo.
+                                        del (Fuormula.stavki[stavk]).literali[crka]
+                                        koliko -= 1
+                                        crka -= 1
+                                else: #if (Tip==Til and vridnastAbjikta==F()) or (Tip==Lit and vridnatAbjikta==T()) #vstavljamo T v Ali -> lahko spustimo ce$
+                                        del Fuormula.stavki[stavk]
+                                        stavkov -= 1
+                                        stavk -= 1
+                                        break
+                        crka += 1
+                stavk += 1
+        #returnat nam ni treba nic, ker gre za spremembo formule na mestu.
+
+
+
+def dpll_brez_ciste(dieFormel):
+        """ Sprejme formulo v CNF obliki in pove, ali ji je mogoce zadostiti. 
+                Ce ji je, vrne slovar potrebnih vrednosti spremenljivk. """
+
+        slovarc = {}
+
+        ### Prva (in edina) cista pojavitev; takoalitako si je treba zapomniti vse nastopajoce spremenljivke, zato zaradi nje ne bo trajalo pretirano dlje:
+        pojavitve = {}
+        for stavk in dieFormel.stavki:
+                for lit in stavk.literali:
+                        S = lit.ime
+                        if S in pojavitve:
+                                pojavitve[S].add(type(lit))
+                        else:
+                                pojavitve[S] = {type(lit)}
+        for i in pojavitve:
+                if len(pojavitve[i])==1:
+                        tip = pojavitve[i].pop()
+                        dodaj(tip(i), T(), slovarc)
+                        zamenjaj(dieFormel, i, slovarc[i])
+        ###
+
+        def pomozna(formula, slovar):
+                #formula je cnf oblike
+                sprememba = True
+                while sprememba:
+                        if formula.stavki==[]:
+                                return (T(), slovar)
+                        else:
+                                for stavek in formula.stavki:
+                                        sprememba = False
+                                        if stavek.literali==[]:
+                                                return (F(), slovar)
+                                        elif len(stavek.literali)==1:  #Nasli smo stavek, ki je kar literal.
+                                                spremenljivka = stavek.literali[0]
+                                                #Nastavimo ustrezno vrednost spremenljivke:
+                                                dodaj(spremenljivka, T(), slovar)
+                                                zamenjaj(formula, spremenljivka.ime, slovar[spremenljivka.ime])
+                                                sprememba = True
+                                                break
+
+                #Poglejmo, ali je ostala se kaksna spremenljivka brez vrednosti:
+                nasliNovo = False
+                for s in formula.stavki:  #Poisces eno spremenljivko, ki se ni v slovarju, tj. ji vrednost se ni dolocena.
+                        for l in s.literali:
+                                nasliNovo = True
+                                break
+                        if nasliNovo: break
+
+                if nasliNovo: #to se mora zgodit zmeraj
+                        formula2 = Cnf([Stavek([litral for litral in st.literali]) for st in formula.stavki]) #naredimo kopijo, saj zadevo kasneje spreminja$
+                        slovar2 = {i:slovar[i] for i in slovar} #naredimo kopijo, saj zadevo kasneje spreminjamo 'na mestu'
+                        slovar2[l.ime] = T()
+                        zamenjaj(formula2, l.ime, T())
+                        blabla = pomozna(formula2, slovar2)
+                        if blabla[0]==T():
+                                return blabla
+                        else:
+                                slovar[l.ime] = F()
+
+                                zamenjaj(formula, l.ime, F())
+                                return pomozna(formula, slovar)
+                else: #to se ne zgodi
+                        print("Sth is wrong...")
+
+        rezultat = pomozna(dieFormel, slovarc)
+
+        #Nastimamo se vse nepotrebne zadeve v slovarju:
+        for teja in pojavitve:
+                if teja not in rezultat[1]:
+                        rezultat[1][teja] = T()
+
+        if rezultat[0]==T():
+                return rezultat[1] #Returnamo slovarcek.
+        else:
+                return 0
+
+
+
+
+
---
+++ primerjavaCasov.py
@@ -0,0 +1,136 @@
+from random import *
+from dpll import dpll
+from dpll_ena_cista_pojavitev import *
+from dpll_brez import dpll_osnoven
+from sudoku import *
+from time import clock
+
+
+print("Primerjava DPLL algoritmov z, brez, ter z le eno začetno preverbo čiste pojavitve. \n")
+D = input("Primerjam DPLL s čisto pojavitvijo? (y/n)  ")
+D1 = input("Primerjam DPLL z le eno izvedbo čiste pojavitve (takoj na začetku)?  (y/n)  ")
+DB = input("Primerjam osnoven DPLL, brez implementacije čiste pojavitve? (y/n)  ")
+L = 0
+if not (D=="n" and D1=="n" and DB=="n"):
+	L = int(input("Koliko naključnih polnitev polj (sudokujev) zgeneriram? Vnesi naravno št."))
+
+if not (D=="n" and D1=="n" and DB=="n"):
+	print("\n Prazen sudoku:")
+	print("="*25)
+if D=="y":
+	CNF = sudoku([]).cnf()
+	before1 = clock()
+	rez1 = dpll(CNF)
+	after1 = clock()
+	print("Dpll z večkratno čisto pojavitvijo potrebuje {0} sec.".format(after1-before1))
+
+if D1=="y":
+	CNF = sudoku([]).cnf()
+	before2 = clock()
+	rez2 = dpll_brez_ciste(CNF)
+	after2 = clock()
+	print("Dpll z enkratno čisto pojavitvijo potrebuje {0} sec.".format(after2-before2))
+
+if DB=="y":
+	CNF = sudoku([]).cnf()
+	before3 = clock()
+	rez3 = dpll_osnoven(CNF)
+	after3 = clock()
+	print("Dpll brez vsakršne čiste pojavitve potrebuje {0} sec.".format(after3-before3))
+
+
+
+for i in range(L): #koliko primerov bomo naredili
+	P = randint(1, 30) #koliko  bo danih polj
+	polja = []
+	p = 0
+	while p<P:
+		vrstica = randint(1,9) #zgeneriramo dana polja in njih vrednosti
+		stolpec = randint(1,9)
+		st = randint(1,9)
+		polje = (vrstica, stolpec, st)
+		if polje not in polja:
+			polja.append(polje)
+			p += 1
+
+	print("\n \n Sudoku {0}: ".format(i+1))
+	print("="*25)
+	print("Podana zasedena polja na začetku: ")
+	print(polja, "\n")
+
+	if D=="y":
+		CNF = sudoku(polja).cnf()
+		before1 = clock()
+		rez = dpll(CNF)
+		after1 = clock()
+		print("Dpll z večkratno čisto pojavitvijo potrebuje {0} sec.".format(after1-before1))
+
+	if D1=="y":
+		CNF = sudoku(polja).cnf()
+		before2 = clock()
+		rez = dpll_brez_ciste(CNF)
+		after2 = clock()
+		print("Dpll z enkratno čisto pojavitvijo potrebuje {0} sec.".format(after2-before2))
+
+	if DB=="y":
+		CNF = sudoku(polja).cnf()
+		before3 = clock()
+		rez = dpll_osnoven(CNF)
+		after3 = clock()
+		print("Dpll brez vsakršne čiste pojavitve potrebuje {0} sec.".format(after3-before3))
+
+	if rez==0:
+		print("Sudoku ni bil rešljiv. \n \n")
+	else:
+		print("Sudoku je bil rešljiv. \n \n")
+	
+
+
+
+#Ker bodo najverjetneje vsi sudokuji, ki bodo takole na random zgenerirani, neresljivi, sledi se 5 primerov
+#sudokujev z interneta, ki pa so zagotovo resljivi:
+
+#easy:
+s1 = [(1,2,1),(1,3,7),(1,4,5),(2,2,2),(2,4,7),(2,6,9),(2,7,8),(3,1,4),(3,4,3),(3,5,8),(3,9,5),(4,2,5),(4,4,2),(4,5,4),(4,6,6),(4,8,3),(4,9,8),(5,4,8),(5,6,3),(6,1,3),(6,2,8),(6,4,1),(6,5,5),(6,6,7),(6,8,4),(7,1,1),(7,5,7),(7,6,2),(7,9,3),(8,3,2),(8,4,9),(8,6,5),(8,8,8),(9,6,8),(9,7,2),(9,8,7)]
+#medium:
+s2 = [(1,3,3),(1,6,6),(1,9,8),(2,1,7),(2,3,4),(2,4,2),(2,5,5),(3,4,7),(3,6,9),(3,7,1),(4,5,9),(4,8,7),(4,9,4),(5,1,4),(5,3,1),(5,5,8),(5,7,6),(5,9,9),(6,1,8),(6,2,9),(6,5,2),(7,3,8),(7,4,3),(7,6,2),(8,5,6),(8,6,5),(8,7,2),(8,9,7),(9,1,6),(9,4,9),(9,7,3)]
+#hard:
+s3 = [(1,4,2),(1,7,7),(2,1,8),(2,4,7),(2,8,2),(3,2,2),(3,3,5),(3,8,1),(3,9,9),(4,2,4),(4,5,3),(4,6,1),(5,3,7),(5,7,9),(6,4,8),(6,5,4),(6,8,6),(7,1,4),(7,2,7),(7,7,5),(7,8,8),(8,2,8),(8,6,2),(8,9,3),(9,3,6),(9,6,5)]
+#evil:
+s4 = [(1,3,2),(1,4,7),(2,3,1),(2,6,4),(2,7,6),(2,8,2),(3,1,4),(3,6,6),(4,1,5),(4,8,4),(4,9,2),(5,2,7),(5,5,8),(5,8,3),(6,1,3),(6,2,2),(6,9,1),(7,4,3),(7,9,5),(8,2,8),(8,3,7),(8,4,5),(8,7,2),(9,6,8),(9,7,9)]
+#veclicno resljiv:
+s5 = [(1,2,1),(1,3,7),(1,4,5),(2,2,2),(2,4,7),(2,6,9),(2,7,8),(3,1,4),(3,4,3)]
+
+
+sud = [sudoku(s1), sudoku(s2), sudoku(s3), sudoku(s4), sudoku(s5)]
+sudi = ["Easy", "Medium", "Hard", "Evil", "večlično rešljiv"]
+
+
+if not (D=="n" and D1=="n" and DB=="n"):
+	print("\n \n Dodatnih 5 rešljivih sudokujev: ")
+	for i in range(5):
+		print("\n \n Sudoku {0}: ".format(i+L+1), sudi[i])
+		print("="*30)
+
+		if D=="y":
+			CNF = sud[i].cnf()
+			bef1 = clock()
+			rez1 = dpll(CNF)
+			aft1 = clock()
+			print("Dpll z večkratno čisto pojavitvijo potrebuje {0} sec.".format(aft1-bef1))
+
+		if D1=="y":
+			CNF = sud[i].cnf()
+			bef2 = clock()
+			rez2 = dpll_brez_ciste(CNF)
+			aft2 = clock()
+			print("Dpll z enkratno čisto pojavitvijo potrebuje {0} sec. ".format(aft2-bef2))
+
+		if DB=="y":
+			CNF = sud[i].cnf()
+			bef3 = clock()
+			rez3 = dpll_osnoven(CNF)
+			aft3 = clock()
+			print("Dpll brez vsakršne čiste pojavitve potrebuje {0} sec. \n \n".format(aft3-bef3))
+
+	
