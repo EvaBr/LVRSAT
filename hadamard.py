851ccbafbaef490d961396cf203d5cb3c6ddf403
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-05-11 22:09:34 +0200

    Spisano ogrodje Hadamardka </3.

--- hadamard.py
+++ hadamard.py
@@ -36,16 +36,13 @@ def hadamard(N):
             return In(prod(i, j, a-1, 0), Ali(n(a,i), n(a,j)))
         else: #a>=b, b>0,  
             return Ali(In(prod(i, j, a-1, b-1), s(a,i), s(a,j)), In(prod(i, j, a-1, b), Ali(n(a,i), n(a,j))))
-    #if N%2==1:
-    #    return F()
-    #else:
-    #    return In(*tuple(prod(i, j) for i in range(2, N+1) for j in range(1, i)))
-    #
-    # Bi rekla, da je tole sehr kurz und elegantisch :P, aber Piton pravi "RuntimeError: maximum recursion depth exceeded". 
+
+    # Bi rekla, da je tole sehr kurz und elegantisch :P, aber Piton posredno
+    # pravi "RuntimeError", ko dozivi katarzo ze med racunanjem za n=10. 
     #
     # Dejmo bit torej malo bolj dinamicni in prostorsko potratni:
-    
-    #prod = [Ali(n(1,i), n(1,j)), In(s(1,i), s(1,j))]+[0]*
+    #
+    #prod = [Ali(n(1,i), n(1,j)), In(s(1,i), s(1,j))]+[0]*... Se pride...
 
     if N%2==1:
         return F()
