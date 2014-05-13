82ea6b6e9e4a953f6437dfff7caf1928daa85092
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2014-05-13 10:55:42 +0200

    Zdaj dela tudi nerekurzivna zadevca, ki za n=22 poracuna v 8sec. Rekurzija sucks.

--- hadamard.py
+++ hadamard.py
@@ -41,7 +41,11 @@ def hadamard(N):
         else: #a>=b, b>0,  
             return Ali(In(prod(i, j, a-1, b-1), equiv(s(a,i), s(a,j))), In(prod(i, j, a-1, b), xor(s(a,i), s(a,j))))
 
-    if N%2==1:
+    if N<=0:
+        return "Haha, you are so funny."
+    elif N==1:
+        return s(1,1)
+    elif N%2==1:
         return F()
     else:
         return In(*tuple(prod(i, j, N, N//2) for i in range(2, N+1) for j in range(1, i)))
@@ -49,31 +53,32 @@ def hadamard(N):
 
 
 # Bi rekla, da je tole sehr kurz und elegantisch :P, aber Piton posredno
-# pravi "RuntimeError", ko dozivi katarzo ze med racunanjem za n=4. 
+# pravi "RuntimeError", ko dozivi katarzo ze med racunanjem za n=10. 
 #
 # Dejmo bit torej malo bolj dinamicni in prostorsko potratni, morda bo hitreje:
     
-def hadamard2(N):
-    if N==1:
+def hadamard2(n):
+
+    def C(i,j,N):
+        #Za vsaka dva razl. stolpca izracunamo C_{n,n/2}.
+        pol = N//2
+        prod = [xor(s(1,i), s(1,j)), equiv(s(1,i), s(1,j))] + [F()]*(pol-1) #Prva vrsta. pol+1 stolpcev.
+        for vrsta in range(2, N): #V zadnji, n-ti vrsti bomo izacunali le zeljeno mesto.
+            for stolp in range(min(vrsta,pol), 0, -1): #0-to mesto posebej, ker je druga formula.
+                prod[stolp] = Ali(In(prod[stolp-1], equiv(s(vrsta,i), s(vrsta,j))), In(prod[stolp], xor(s(vrsta,i), s(vrsta,j))))
+            prod[0] = In(prod[0], xor(s(vrsta,i), s(vrsta,j)))
+        #Zadnja vrstica - vrsta=N: potrebujemo le stvar na N//2-tem mestu...
+        C = Ali(In(prod[pol-1], equiv(s(N,i), s(N,j))), In(prod[pol], xor(s(N,i), s(N,j))))
+        return C
+
+    if n<=0:
+        return "Haha, you are so funny."
+    elif n==1:
         return s(1,1)
-    elif N%2==1:
+    elif n%2==1:
         return F()
     else:
-        st = 1
-        pol = N//2
-        for i in range(2, N+1):
-            for j in range(1, i):
-                #Za vsaka dva razl. stolpca izracunamo C_{n,n/2}.
-                prod = [xor(s(1,i), s(1,j)), equiv(s(1,i), s(1,j))] + [F()]*(pol-1) #Prva vrsta.
-                for vrsta in range(2, N): #V zadnji, n-ti vrsti bomo izacunali le zeljeno mesto.
-                    for stolp in range(vrsta, 0, -1): #0-to mesto posebej, ker je druga formula.
-                        prod[stolp] = Ali(In(prod[stolp-1], equiv(s(vrsta,i), s(vrsta,j))), In(prod[stolp], xor(s(vrsta,i), s(vrsta,j))))
-                    prod[0] = In(prod[0], xor(s(vrsta,i), s(vrsta,j)))
-                #Zadnja vrstica - vrsta=N: potrebujemo le stvar na N//2-tem mestu...
-                C = Ali(In(prod[pol-1], equiv(s(N,i), s(N,j))), In(prod[pol], xor(s(N,i), s(N,j))))
-                if st==1:
-                    rez = C
-                    st = 0
-                else:
-                    rez = In(rez, C)
-        return rez        
+        return In(*tuple(C(i,j,n) for i in range(2, n+1) for j in range(1,i)))        
+
+
+# Potrata casa: hadamard(10) 4sec, in hadamard2(10) 0.18 sec! WIN :D
